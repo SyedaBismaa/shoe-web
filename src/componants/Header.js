361343490c8d02874179b768712f2d919de6f0c3
Syed Bimsa <Syeda.Bisma.Fatima.1@gmail.com>
2024-07-26 00:17:17 +0530

    Online-Oflline feature added

--- src/componants/Header.js
+++ src/componants/Header.js
@@ -1,20 +1,24 @@
-const Header=()=>{
-    return (
-        <div>
-  
-       <div className=" justify-between items-center flex">
-           <img className="logo" src="https://static.vecteezy.com/system/resources/previews/005/020/444/original/modern-sneaker-shoe-logo-vector.jpg"/>
-           <ul>
-           <li>Home</li>
-           <li>About Us</li>
-           <li>Contact Us</li>
-           <li>Cart</li>
-           </ul>              
-           </div>
-         
 
-        </div>
-    )
-}
+import React from "react";
+import { useOnline } from "./utils/UseOnline";
+
+const Header = () => {
+  const OnlineStatus = useOnline();
+
+  return (
+    <div>
+      <div className="justify-between items-center flex">
+        <img className="logo" src="https://static.vecteezy.com/system/resources/previews/005/020/444/original/modern-sneaker-shoe-logo-vector.jpg" alt="Logo" />
+        <ul>
+          <li>OnlineStatus {OnlineStatus ? "🟢" : "🔴"}</li>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
